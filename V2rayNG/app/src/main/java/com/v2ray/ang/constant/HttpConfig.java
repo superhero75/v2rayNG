28a141ffa5d76c24edf8220e38b76889cf34a843
vincent fong <vwpfong@gmail.com>
2019-11-04 03:12:35 +0800

    login

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/constant/HttpConfig.java
@@ -0,0 +1,7 @@
+package com.v2ray.ang.constant;
+
+public class HttpConfig {
+    public static final String URL_PATH = "https://my.oo7.app/modules/servers/V2raySocks/api.php";
+    public static final String KEY  = "$";
+    public static final String UCKEY = "o";
+}
---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/util/NetUtils.java
@@ -0,0 +1,148 @@
+package com.v2ray.ang.util;
+
+import android.accounts.NetworkErrorException;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.v2ray.ang.constant.HttpConfig;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetUtils {
+    private static final String TAG = NetUtils.class.getSimpleName();
+
+    public static String post(String url, String content) {
+        HttpURLConnection conn = null;
+        try {
+            // 创建一个URL对象
+            URL mURL = new URL(url);
+            // 调用URL的openConnection()方法,获取HttpURLConnection对象
+            conn = (HttpURLConnection) mURL.openConnection();
+
+            conn.setRequestMethod("POST");// 设置请求方法为post
+            conn.setReadTimeout(30000);// 设置读取超时为5秒
+            conn.setConnectTimeout(30000);// 设置连接网络超时为10秒
+            conn.setDoOutput(true);// 设置此方法,允许向服务器输出内容
+
+            // post请求的参数
+            String data = content;
+            // 获得一个输出流,向服务器写数据,默认情况下,系统不允许向服务器输出内容
+            OutputStream out = conn.getOutputStream();// 获得一个输出流,向服务器写数据
+            out.write(data.getBytes());
+            out.flush();
+            out.close();
+
+            int responseCode = conn.getResponseCode();// 调用此方法就不必再使用conn.connect()方法
+            if (responseCode == 200) {
+
+                InputStream is = conn.getInputStream();
+                String response = getStringFromInputStream(is);
+                return response;
+            } else {
+                throw new NetworkErrorException("response status is "+responseCode);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.disconnect();// 关闭连接
+            }
+        }
+
+        return null;
+    }
+
+    public static String get(String url) {
+        HttpURLConnection conn = null;
+        try {
+            // 利用string url构建URL对象
+            URL mURL = new URL(url);
+            conn = (HttpURLConnection) mURL.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setReadTimeout(30000);
+            conn.setConnectTimeout(30000);
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+
+                InputStream is = conn.getInputStream();
+                String response = getStringFromInputStream(is);
+                return response;
+            } else {
+                throw new NetworkErrorException("response status is "+responseCode);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return null;
+    }
+
+    private static String getStringFromInputStream(InputStream is)
+            throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // 模板代码 必须熟练
+        byte[] buffer = new byte[1024];
+        int len = -1;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+        is.close();
+        String state = os.toString();// 把流中的数据转换成字符串,采用的编码是utf-8(模拟器默认编码)
+        os.close();
+        return state;
+    }
+
+    @NonNull
+    public static JsonObject getConfigJsonFromServer(String email, String password) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "Email is " + email + " mPassword is " + password + " " + jsonObject.toString());
+
+        String encode = Authcode.Encode(jsonObject.toString(), HttpConfig.UCKEY);
+
+        Log.d(TAG, "encode is " + encode);
+
+        String response = get(HttpConfig.URL_PATH + "?data=" + encode);
+
+        Log.d(TAG, "response is " + response);
+
+        if (response != null) {
+            String licenseDecode = LicenseUtils.licenseDecode(response, HttpConfig.KEY);
+            String decode = Authcode.Decode(licenseDecode, HttpConfig.UCKEY);
+            Log.d(TAG, "decode is " + decode);
+            JsonObject json = (JsonObject) new JsonParser().parse(decode);
+            String result = json.get("result").getAsString();
+            Log.d(TAG, "result is " + result);
+            return json;
+        } else {
+            JsonObject json = new JsonObject();
+            json.addProperty("result", "failure");
+            return json;
+        }
+    }
+}
